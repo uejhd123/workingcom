4058f6dd00a3c8696121bd3541b13282ccc21987
Kirill Akulenko <127042135+uejhd123@users.noreply.github.com>
2024-05-16 01:44:12 +0700

    fadsfjsd

--- start.sh
+++ start.sh
@@ -1,38 +1,28 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt install python3.10-venv -y
-sudo apt install nginx -y
-sudo apt install git -y
-sudo apt install python3-pip -y
-sudo apt install mysql-server -y
-sudo apt-get install python3-dev default-libmysqlclient-dev build-essential -y
-sudo apt install pkg-config -y
-sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
-sudo apt install -y python3-venv
+rm -rf /usr/share/nginx/html/*
 
 
-echo "CREATE USER if not exists 'workingcomAdmin'@'localhost' IDENTIFIED BY 'Admin_1234';
-GRANT ALL PRIVILEGES ON *.* TO 'workingcomAdmin'@'localhost' WITH GRANT OPTION;
-CREATE DATABASE if not exists workingcom;
-FLUSH PRIVILEGES;
-" > db.sql
-sudo mysql < db.sql
+MY_PUBLIC_IP=$(curl https://api.ipify.org)
+echo "\033[47m\033[31m{$MY_PUBLIC_IP}\033[0m"
 
+
+echo -e "\033[47m\033[31mактивация venv\033[0m"
 cd /usr/share/nginx/html
 python3  -m venv venv
 git clone https://github.com/uejhd123/workingcom.git
 sudo mv workingcom/workingcomlayout2-main/* ./
 sudo rm -rf workingcom/workingcomlayout2-main workingcom/work
+source venv/bin/activate 
 
-source venv/bin/activate
-echo "venv activate"
 sed -i 's/pywin32==306//g' workingcom/requirments.txt
-sed -i 's/ALLOWED_HOSTS = ['178.154.227.55', 'localhost', '127.0.0.1', 'localhost:7000']/ALLOWED_HOSTS = ['178.154.227.55', 'localhost']/g' workingcom/workingcom/settings.py
+sed -i "s/ALLOWED_HOSTS = []/ALLOWED_HOSTS = [$MY_PUBLIC_IP, 'localhost']/g" workingcom/workingcom/settings.py
 pip3 install -r workingcom/requirments.txt
 sed  -i 's/root \/var\/www\/html/root \/usr\/share\/nginx\/html/g' /etc/nginx/sites-available/default
 systemctl restart nginx
-sed -i 's/localhost:8000/ВСТАВИТЬ АДРЕС/g' js/*.js
-sed -i 's/localhost:7000/ВСТАВИТЬ АДРЕС/g' js/*.js
+ sed -i "s/localhost:8000/$MY_PUBLIC_IP:8000/g" js/*.js
+ sed -i "s/localhost:7000/$MY_PUBLIC_IP/g" js/*.js
+echo -e "\033[47m\033[31mприменение sed завершено\033[0m"
 
+echo -e "\033[47m\033[31mзапуск проекта\033[0m"
 python3 workingcom/manage.py makemigrations
 python3 workingcom/manage.py migrate
 nohup python3 workingcom/manage.py runserver 0.0.0.0:8000
